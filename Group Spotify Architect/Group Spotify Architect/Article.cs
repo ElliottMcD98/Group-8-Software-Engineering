2cd23ac80b5703c81e36a6b855c3c46d10f64f13
Callum Dyson-Gainsborough <cgainsborough@hotmail.com>
2019-10-24 15:48:54 +0100

    API's
    Added Spotify API
    Added News API
    Created Basic test gui
    Added Functions to test API's
    Added wrapper objects for returned items

---
+++ Group Spotify Architect/Group Spotify Architect/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Spotify_Architect
+{
+    class Article
+    {
+        public string Title;
+        public string Desc;
+        public string Link;
+        public Article(string Title, string Desc, string Link)
+        {
+            this.Title = Title;
+            this.Link = Link;
+            this.Desc = Desc;
+        }
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ Group Spotify Architect/Group Spotify Architect/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_Spotify_Architect
+{
+    public partial class Form1 : Form
+    {
+        private SpotifyInterface spotify;
+        private NewsInterface news;
+        public Form1()
+        {
+            InitializeComponent();
+            spotify = new SpotifyInterface();
+            news = new NewsInterface();
+        }
+
+        private void bt_connect_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                spotify.Connect();
+                lb_connected.Text = "Connected";
+            }
+            catch (Exception err)
+            {
+                lb_connected.Text = $"Error: {err.Message}";
+            }
+        }
+
+        private async void bt_GetNews_Click(object sender, EventArgs e)
+        {
+            var tmp = await news.GetHeadlines(NewsAPI.Constants.Countries.GB);
+            lb_News.Items.Clear();
+            lb_News.Items.AddRange(tmp.ToArray());
+        }
+
+        private async void bt_search_Click(object sender, EventArgs e)
+        {
+            var search = tb_search.Text;
+            var results = await spotify.GetSongs(search);
+            lb_spotify.Items.Clear();
+            lb_spotify.Items.AddRange(results.ToArray());
+        }
+    }
+}
