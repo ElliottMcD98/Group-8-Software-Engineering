75ef9312a247a9c35aff3878f290477ce32599c1
alexandjstaniforth <57140260+alexandjstaniforth@users.noreply.github.com>
2019-11-04 21:38:02 +0000

    Inital User Interface
    First version of the windows user interface based on UI design

--- Group Spotify Project/Group Spotify Architect/Program.cs
+++ Group Spotify Project/Group Spotify Architect/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Group_Spotify_Architect
+namespace SpotifyDesktop
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Group_Spotify_Architect
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SearchForm());
         }
     }
 }
