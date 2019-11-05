b0710ad9db0039159f9b0416d7f75a5e20ad8726
alexandjstaniforth <57140260+alexandjstaniforth@users.noreply.github.com>
2019-11-05 22:08:49 +0000

    change to use SearchForm

--- Group Spotify Project/Group Spotify Architect/Program.cs
+++ Group Spotify Project/Group Spotify Architect/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
+using Group_Spotify_Architect;
 
 namespace SpotifyDesktop
 {
