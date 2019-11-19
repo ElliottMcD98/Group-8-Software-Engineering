acafa5530f04028a972b29252302948a25858966
Alex Staniforth <15610188@students.lincoln.ac.uk>
2019-11-19 21:54:15 +0000

    Inital Unit Testing

---
+++ Group Spotify Project/SpotifyAppNUnitTestProject/ArticleUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+
+namespace Group_Spotify_Architect
+{
+    public class ArticleUnitTests
+    {
+        
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckTitleIsReturnedCorrectly()
+        {
+            const string ExpectedTitle = "Test Title";
+
+            var sut = new Article(ExpectedTitle, "Test Descr","Test Link");
+            Assert.AreEqual(ExpectedTitle, sut.Title);
+        }
+        [Test]
+        public void CheckDescParamIsReturnedCorrectly()
+        {
+            const string ExpectedDescription = "Test Description"; 
+
+            var sut = new Article("testt", ExpectedDescription, "Test Link");
+            Assert.AreEqual(ExpectedDescription, sut.Desc);
+        }
+
+        [Test]
+        public void CheckLinkParamIsReturnedCorrectly()
+        {
+            const string ExpectedLink = "Test Link";
+
+            var sut = new Article("testt", "Description", ExpectedLink);
+            Assert.AreEqual(ExpectedLink, sut.Link);
+        }
+
+
+    }
+}
