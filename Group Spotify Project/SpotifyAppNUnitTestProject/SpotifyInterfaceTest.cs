41e4c29aff075259945d5c27e92d8b114fefa28c
Alex Staniforth <15610188@students.lincoln.ac.uk>
2019-11-21 20:58:21 +0000

    Added comments to unit test as it was found the code could not be unit tested in its current form

--- Group Spotify Project/SpotifyAppNUnitTestProject/SpotifyInterfaceTest.cs
+++ Group Spotify Project/SpotifyAppNUnitTestProject/SpotifyInterfaceTest.cs
@@ -12,7 +12,7 @@ namespace SpotifyAppNUnitTestProject
         public void Test1()
         {
             //on investigation it was found that SpotifyInterface wasn't testible as it stands
-            //it would need major changes
+            //it would need major changes to inject the webservices SpotifyWebAPI into the class so that it can be mocked
             var songs = new string[]
             {
                 "Song1","Song2","Song3"
